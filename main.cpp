e7b3dc4c9eb47bc7384e080c6146463df56bec94
ben <benyamin@ai-blox.com>
2024-04-08 17:21:47 +0200

    update and revert back to the previous checkout

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "Object.h"
 #include "Item.h"
 #include <stdlib.h>
-#include <string.h>
 
 int main(){
     std::cout << "hello world\n";
@@ -17,17 +16,7 @@ int main(){
     std::cout << obj1->get_distance() << std::endl;
     std::cout << obj2->get_distance() << std::endl;
 
-    delete obj1;
-    delete obj2;
+    char *byte = calloc(sizeof(int) * 10, 0);
 
-    char *byte = (char*)malloc(sizeof(char) * 10);
-    strcpy(byte, "beny");
-    printf("%s\n", byte);
-
-    delete byte;
-
-    Item *new_item = Item(1, 2, 3.6, 6, 9);
-    std::cout << new_item->get_middle() << std::endl;
-    std::cout << new_item->get_front_distance() << std::endl;
     return 0;
 }
