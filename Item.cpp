5bcf7930bc6523da24da5417230e307e28151404
ben <benyamin@ai-blox.com>
2024-05-14 13:32:13 +0200

    update

--- Item.cpp
+++ Item.cpp
@@ -17,5 +17,6 @@ Item::Item(int prev, int next, int front, double middle):Object(prev, next){
     this->front = front;
 }
 int Item::get_front_distance(){
+    // This is the check to see how git detects this
     return (this->front - (int)this->middle);
 }
